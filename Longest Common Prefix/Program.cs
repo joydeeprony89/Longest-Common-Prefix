581b5371b761d1fe52db85d85506e2401c1c1ca7
jd_786 <jaydeep_shil@certree.com>
2021-12-28 23:09:01 +0530

    Add project files.

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Longest_Common_Prefix
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      string[] strs = new string[] { "flow", "fly", "flower" };
+      Console.WriteLine(p.LongestCommonPrefix(strs));
+    }
+
+    public string LongestCommonPrefix(string[] strs)
+    {
+      if (strs == null || strs.Length == 0) return "";
+      string prefix = strs[0];
+      for (int i = 1; i < strs.Length; i++)
+      {
+        while (!strs[i].StartsWith(prefix))
+        {
+          prefix = prefix.Substring(0, prefix.Length - 1);
+        }
+      }
+
+      return prefix;
+    }
+  }
+}
